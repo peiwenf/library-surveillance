f01c569017083160d55165e4da41f65a0801cb60
peiwenf <72475655+peiwenf@users.noreply.github.com>
2023-03-06 14:20:21 -0800

    Update example.js

--- example.js
+++ example.js
@@ -8,7 +8,7 @@ const { join } = require("path");
   let OUT_DIR = true;
 
   // The URL to test
-  const URL = "${{ github.event.inputs.url }}";
+  const URL = process.argv[2];
   const inUrl = URL.startsWith("http") ? URL : `http://${URL}`;
 
   const defaultConfig = {
@@ -18,41 +18,14 @@ const { join } = require("path");
     emulateDevice: EMULATE_DEVICE,
   };
 
-//   const result = await collector(
-//     OUT_DIR
-//       ? { ...defaultConfig, ...{ outDir: join(__dirname, URL) } }
-//       : defaultConfig
-//   );
-//   if (OUT_DIR) {
-//     console.log(
-//       `For captured data please look in ${join(__dirname, URL)}`
-//     );
-//   }
-// })();
-  if (OUT_DIR) {
-    const artifactName = `${URL}-${process.env.BUILD_ID}.tar.gz`;
-    const artifactPath = join(__dirname, URL, `${URL}.tar.gz`);
-    console.log(`Saving artifact ${artifactName}`);
-    console.log(`Artifact path: ${artifactPath}`);
-    await uploadArtifact(artifactName, [artifactPath]);
-  }
-
+  const result = await collector(
+    OUT_DIR
+      ? { ...defaultConfig, ...{ outDir: join(__dirname, URL) } }
+      : defaultConfig
+  );
   if (OUT_DIR) {
     console.log(
       `For captured data please look in ${join(__dirname, URL)}`
     );
   }
 })();
-
-async function uploadArtifact(name, files) {
-  const artifact = {
-    name: name,
-    path: files,
-  };
-  try {
-    const artifactClient = artifact.create();
-    await artifactClient.uploadArtifact(name, files, __dirname);
-  } catch (error) {
-    console.log(error);
-  }
-}
