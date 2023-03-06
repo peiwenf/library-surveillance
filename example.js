6faefe1ee2278dfe014fc53634c9e129d5d68f57
peiwenf <72475655+peiwenf@users.noreply.github.com>
2023-03-06 14:08:56 -0800

    Update example.js

--- example.js
+++ example.js
@@ -18,14 +18,41 @@ const { join } = require("path");
     emulateDevice: EMULATE_DEVICE,
   };
 
-  const result = await collector(
-    OUT_DIR
-      ? { ...defaultConfig, ...{ outDir: join(__dirname, URL) } }
-      : defaultConfig
-  );
+//   const result = await collector(
+//     OUT_DIR
+//       ? { ...defaultConfig, ...{ outDir: join(__dirname, URL) } }
+//       : defaultConfig
+//   );
+//   if (OUT_DIR) {
+//     console.log(
+//       `For captured data please look in ${join(__dirname, URL)}`
+//     );
+//   }
+// })();
+  if (OUT_DIR) {
+    const artifactName = `${URL}-${process.env.BUILD_ID}.tar.gz`;
+    const artifactPath = join(__dirname, URL, `${URL}.tar.gz`);
+    console.log(`Saving artifact ${artifactName}`);
+    console.log(`Artifact path: ${artifactPath}`);
+    await uploadArtifact(artifactName, [artifactPath]);
+  }
+
   if (OUT_DIR) {
     console.log(
       `For captured data please look in ${join(__dirname, URL)}`
     );
   }
 })();
+
+async function uploadArtifact(name, files) {
+  const artifact = {
+    name: name,
+    path: files,
+  };
+  try {
+    const artifactClient = artifact.create();
+    await artifactClient.uploadArtifact(name, files, __dirname);
+  } catch (error) {
+    console.log(error);
+  }
+}
