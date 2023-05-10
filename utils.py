35640f7d0f161e0ae718bdddc60a7a99b95e3863
peiwenf <72475655+peiwenf@users.noreply.github.com>
2023-05-09 18:16:46 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,11 +4,9 @@
 
 def clean_url(url: str) -> str:
     url = url.replace("https://", "").replace("http://", "")
-    # Find the index of the first "/"
     index = url.find("/")
     if index != -1:
         url = url[:index]
 #     if url.endswith("/"):
 #         url = url[:-1]
-
     return url
