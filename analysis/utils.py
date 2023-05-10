a345b9183ac2cbe84c0c48d2a53a12478a20ca1a
peiwenf <72475655+peiwenf@users.noreply.github.com>
2023-05-09 18:17:46 -0700

    Update utils.py

--- analysis/utils.py
+++ analysis/utils.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
+# def clean_url(url: str) -> str:
+#     url = url.replace("https://", "").replace("http://", "")
+#     if url.endswith("/"):
+#         url = url[:-1]
+
+#     return url
+
 def clean_url(url: str) -> str:
     url = url.replace("https://", "").replace("http://", "")
-    if url.endswith("/"):
-        url = url[:-1]
-
+    index = url.find("/")
+    if index != -1:
+        url = url[:index]
     return url
